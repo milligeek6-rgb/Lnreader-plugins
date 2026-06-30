33685db6e863bbcf674b2b6178dbc53351cb8455
milligeek6-rgb <milligeek6@gmail.com>
2026-06-30 23:22:39 +0000

    Update freewebnovel.js

--- freewebnovel.js
+++ freewebnovel.js
@@ -5,11 +5,11 @@ const cheerio = require("cheerio");
 
 class FreeWebNovelPlugin {
     constructor() {
-        this.id = "freewebnovel";
+        this.id = "freewebnovel_fixed_v3";
         this.name = "Free Web Novel (Fixed)";
         this.icon = "assets/icon.png";
         this.site = "https://freewebnovel.com";
-        this.version = "3.1.5";
+        this.version = "4.0.0";
     }
 
     async popularNovels(pageNo) {
